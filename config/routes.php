dec889a7ba076953eed1dae5e4b09182eabcf165
Simone Gentili <sensorario@gmail.com>
2023-07-19 22:59:54 +0200

    feat: remove application files

--- config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php return [
-
-    // '/ciaone' => App\CiaoneController::class,
-    // '/mondone/:number' => App\MondoneController::class,
-    // '/mon/:done/:number' => App\MondoneController::class,
-
-];
